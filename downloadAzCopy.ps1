f76ab65adf739f33ccca762102206af1a2a21805
duducat531 <33836130+duducat531@users.noreply.github.com>
2019-05-23 19:54:53 +0800

    acd

--- downloadAzCopy.ps1
+++ downloadAzCopy.ps1
@@ -1,7 +1,6 @@
-#Download via URI using SAS
+"start" | Out-File 'D:\log.txt' -Force
 New-Item -ItemType directory -Path D:\temp
-$BlobUri = 'https://loadtestvmss.blob.core.windows.net/azureportaldeploy/MicrosoftAzureStorageAzCopy_netcore_x64.msi'
-$Sas = '?st=2019-05-23T09%3A26%3A48Z&se=2019-06-24T09%3A26%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=XbzDu%2FG3xb7qv84gzwlrZpcnhyBgdXd9SvvgjmhKArM%3D'
 $OutputPath = 'D:\temp\MicrosoftAzureStorageAzCopy_netcore_x64.msi'
-$FullUri = "$BlobUri$Sas"
+$FullUri = "https://loadtestvmss.blob.core.windows.net/azureportaldeploy/MicrosoftAzureStorageAzCopy_netcore_x64.msi?st=2019-05-23T09%3A26%3A48Z&se=2019-06-24T09%3A26%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=XbzDu%2FG3xb7qv84gzwlrZpcnhyBgdXd9SvvgjmhKArM%3D"
 (New-Object System.Net.WebClient).DownloadFile($FullUri, $OutputPath) 
+"end" | Out-File 'D:\log.txt' -Force
