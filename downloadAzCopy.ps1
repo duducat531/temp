93ce2da7e69fdd50666fee9900863f199fdf4efc
duducat531 <33836130+duducat531@users.noreply.github.com>
2019-05-23 17:44:55 +0800

    add file

---
+++ downloadAzCopy.ps1
@@ -0,0 +1,6 @@
+#Download via URI using SAS
+$BlobUri = 'https://loadtestvmss.blob.core.windows.net/azureportaldeploy/MicrosoftAzureStorageAzCopy_netcore_x64.msi'
+$Sas = '?st=2019-05-23T09%3A26%3A48Z&se=2019-06-24T09%3A26%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=XbzDu%2FG3xb7qv84gzwlrZpcnhyBgdXd9SvvgjmhKArM%3D'
+$OutputPath = 'C:\Temp\MicrosoftAzureStorageAzCopy_netcore_x64.msi'
+$FullUri = "$BlobUri$Sas"
+(New-Object System.Net.WebClient).DownloadFile($FullUri, $OutputPath) 
